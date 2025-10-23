70b9ebaa250342a24a368c67734970532bcf6881
jdandridge86 <jdandridge@eagles.bridgewater.edu>
2025-10-23 12:24:48 -0400

    search results (in progress)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,15 @@ import { RouterLink } from 'vue-router';
 import { ref , reactive } from 'vue'
 
 const apiKey = "8091b135029642499cfa2a83e6513777";
+const items = ref([])
 let searchTerm = ref("");
+let maxTime = ref("");
+let recipeSearchLength = ref("");
 
-async function RecipeSearch (searchTerm) {
+async function RecipeSearch (searchTerm, maxTime) {
 	event.preventDefault()
 
-		const url = `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&apiKey=${apiKey}`
+		const url = `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&maxReadyTime=${maxTime}&apiKey=${apiKey}`
 
 		const options = {
 			method: "GET",
@@ -23,6 +26,9 @@ async function RecipeSearch (searchTerm) {
 			
 			let data = await response.json()
 			console.log(data)
+			items.value = data.results;			
+			recipeSearchLength = data.results.length;
+			console.log(recipeSearchLength)
 		}else {
 			console.log("request failed")
 		}
@@ -44,12 +50,22 @@ async function RecipeSearch (searchTerm) {
 
 		<div class="search">
               <label>Find your next recipe:   </label><br>
-              <input type="text" required id="searchTerm" v-model="searchTerm"><br><br>				
+              <input type="text" required id="searchTerm" v-model="searchTerm"><br><br>
+			  
+			  <label>Maximum Cook Time:   </label><br>
+              <input type="number" required id="maxTime" v-model="maxTime"><br><br>
 
               <div class="centeredButton">
-                  <button class="button" @click="RecipeSearch(searchTerm)">Search</button>
+                  <button class="button" @click="RecipeSearch(searchTerm, maxTime)">Search</button>
               </div>
 
+        </div>
+
+		<div class="results">
+              <div v-if="recipeSearchLength === 0">No recipes found</div>
+				<li v-for="item in items">
+                	<p>Recipe Name: {{ item }}</p>
+				</li>
         </div>
 
 
