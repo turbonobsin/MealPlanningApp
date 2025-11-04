e62481a6fa50c31e872e989759f2f8e547f75294
jdandridge86 <jdandridge@eagles.bridgewater.edu>
2025-11-04 10:51:23 -0500

    Update RecipeDetailsView.vue

--- src/views/RecipeDetailsView.vue
+++ src/views/RecipeDetailsView.vue
@@ -1,13 +1,60 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-import { ref , reactive } from 'vue'
+import { ref , reactive, onMounted } from 'vue'
 
 const props = defineProps({ recipeId: String })
 
+const apiKey = "8091b135029642499cfa2a83e6513777";
+const items = ref([])
+
+async function getRecipeDetails() {
+
+	let url = new URL(`https://api.spoonacular.com/recipes/${props.recipeId}/information`);
+    url.searchParams.set("apiKey", apiKey);
+	
+	const options = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	}
+
+	let response = await fetch(url, options)
+
+	if (response.status === 200) {
+
+		let data = await response.json()
+		console.log(data)
+		//items.value = data.results;
+		//recipeSearchLength = data.results.length;
+		//console.log(recipeSearchLength)
+	} else {
+		console.log("request failed")
+	}
+}
+
+onMounted(() => {
+	getRecipeDetails();
+})
 </script>
 
 
 <template>
     <h1>Recipe Details Page</h1>
     <div>RecipeId: {{ recipeId }}</div>
+    <!--Recipe name-->
+    <img :src= "data.image">
+
+
+
+    <!--Image-->
+
+
+    <!--Cook Time, Level, Calories-->
+
+
+    <!--Ingriends-->
+
+
+    <!--Instructions-->
 </template>
